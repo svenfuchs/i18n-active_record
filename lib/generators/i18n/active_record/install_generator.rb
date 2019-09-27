6ad35669e4b165186d109e3b7c97f9a0ddcb28d3
Tim Masliuchenko <insside@gmail.com>
2019-09-27 16:46:53 +0300

    Add i18n:active_record:install

---
+++ lib/generators/i18n/active_record/install_generator.rb
@@ -0,0 +1,27 @@
+require 'rails/generators/active_record'
+
+module I18n
+  module ActiveRecord
+    module Generators
+      class  InstallGenerator < ::ActiveRecord::Generators::Base
+        desc "Installs i18n-active_record and generates the necessary migrations"
+
+        argument :name, type: :string, default: 'Translation'
+
+        class_option :simple, type: :boolean, default: false, desc: 'Perform the simple setup'
+
+        source_root File.expand_path('templates', __dir__)
+
+        def copy_initializer
+          tpl = "#{options[:simple] ? 'simple' : 'advanced'}_initializer.rb.erb"
+
+          template tpl, 'config/initializers/i18n_active_record.rb'
+        end
+
+        def create_migrations
+          migration_template 'migration.rb.erb', 'db/migrate/create_translations.rb'
+        end
+      end
+    end
+  end
+end
