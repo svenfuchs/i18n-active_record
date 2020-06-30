230a89f03c904cd5be0e8f62135d32a0d216d946
Jack Ford <jford8820@gmail.com>
2020-06-30 15:30:09 +0200

    Fix obvious bug

--- lib/i18n/backend/active_record.rb
+++ lib/i18n/backend/active_record.rb
@@ -77,7 +77,7 @@ module I18n
           result = if key == ''
             Translation.locale(locale).all
           else
-            Rails.cache.fetch("i18n-active-record-#{translation.locale}-#{translation.key}") do
+            Rails.cache.fetch("i18n-active-record-#{locale}-#{key}") do
               Translation.locale(locale).lookup(key)
             end
           end
